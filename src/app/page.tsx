b2f230e9750b8672b78a6ac61f6c493d3360eb1b
ThiagoSaraiva-Cods <thiagosaraiva.cods@gmail.com>
2025-08-09 14:23:07 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,48 @@
-import { Header } from "./authentication/components/common/header";
+import Image from "next/image";
 
-export default function Home() {
+import ProductList from "@/components/common/product-list";
+import { db } from "@/db";
+
+import { Header } from "../components/common/header";
+
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+      category: true,
+    },
+  });
+
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner-desktop-1.png"
+            alt="Leve uma vida com estilo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList products={products} title="Mais vendidos" />
+
+        <div className="px-5">
+          <Image
+            src="/banner-desktop-2.png"
+            alt="Seja autêntico"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
+    </>
   );
-}
+};
+
+export default Home;
